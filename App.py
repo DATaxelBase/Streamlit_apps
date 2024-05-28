538d134f77eb66d60b56177742b06f1898205a54
DATaxelBase <112965837+DATaxelBase@users.noreply.github.com>
2024-05-28 15:23:28 +0200

    Update App.py

--- App.py
+++ App.py
@@ -9,9 +9,21 @@ import time
 from bs4 import BeautifulSoup as bf
 
 st.write("""
-# My first app 
-Hello *world!*
+# News Scraper
+Hello, let's scrape your sitemap
 """)
-date = st.date_input("Pick a date")
-#print(date)
-st.write('Date :',date.year, date.month, date.day)
+option = st.selectbox(
+    "What news period want you to scrap?",
+    ("On date setting", "On month and year", "On year"))
+
+if option =="Full date seting":
+  date = st.date_input("Pick a date")
+  st.write('Date :',date.year, date.month, date.day)
+elif option == "On month and year":
+  month = st.number_input("Insert the month")
+  year = st.number_input("Insert the year")
+  st.write('Month :',int(month),'\t Year:', int(year))
+else:
+  year_only = st.number_input("Insert the year")
+  st.write('Year :',year_only)
+  
