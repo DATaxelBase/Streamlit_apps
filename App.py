ed4ab0c431092bbe6ee80e75f0237528e3098dd7
DATaxelBase <112965837+DATaxelBase@users.noreply.github.com>
2024-05-28 16:16:49 +0200

    Update App.py

--- App.py
+++ App.py
@@ -12,18 +12,39 @@ st.write("""
 # News Scraper
 Hello, let's scrape your sitemap
 """)
-option = st.selectbox(
-    "What news period want you to scrap?",
-    ("On date setting", "On month and year", "On year"))
 
-if option =="Full date seting":
-  date = st.date_input("Pick a date")
-  st.write('Date :',date.year, date.month, date.day)
-elif option == "On month and year":
-  month = st.number_input("Insert the month")
-  year = st.number_input("Insert the year")
-  st.write('Month :',int(month),'\t Year:', int(year))
-else:
-  year_only = st.number_input("Insert the year")
-  st.write('Year :',year_only)
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+
+col1, col2 = st.columns(2)
+
+with col1:
+    option = st.selectbox(index = None,key = st.session_state.visibility,
+    "What news period want you to scrap?",
+    ("On date setting", "On month and year", "On year"),placeholder="Choose an option")
+    
+
+
+with col2:
+    if option == "Full date seting":
+        date = st.date_input("Pick a date",key ="visibility")
+        st.write('Date :',date.year, date.month, date.day)
+        month = st.number_input("Insert the month",key =st.session_state.disabled)
+        year = st.number_input("Insert the year",key =st.session_state.disabled)
+        year_only = st.number_input("Insert the year",st.session_state.disabled)
+
+    elif option == "On month and year":
+        date = st.date_input("Pick a date",key =st.session_state.disabled)
+        month = st.number_input("Insert the month",key =st.session_state.visibility)
+        year = st.number_input("Insert the year",key =st.session_state.visibility)
+        year_only = st.number_input("Insert the year",st.session_state.disabled)
+        st.write('Month :',int(month),'\t Year:', int(year))
+    else:
+        date = st.date_input("Pick a date",key =st.session_state.disabled)
+        month = st.number_input("Insert the month",key =st.session_state.disabled)
+        year = st.number_input("Insert the year",key =st.session_state.disabled)
+        year_only = st.number_input("Insert the year",st.session_state.visibility)
+        st.write('Year :',year_only)
   
