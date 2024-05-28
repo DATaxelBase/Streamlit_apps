9996a8de9648c00740e9b5de33892b6e81e0bd06
DATaxelBase <112965837+DATaxelBase@users.noreply.github.com>
2024-05-28 16:52:16 +0200

    Update App.py

--- App.py
+++ App.py
@@ -17,31 +17,24 @@ Hello, let's scrape your sitemap
 col1, col2 = st.columns(2)
 
 with col1:
-    option = st.selectbox(index = None,
+    option = st.radio(
     label = "What news period want you to scrap?",placeholder="Choose an option",
     options =("On date setting", "On month and year", "On year"))
     
 
 
 with col2:
-    month = st.number_input("Insert the month",disabled = True)
-    year = st.number_input("Insert the year",disabled = True)
-    year_only = st.number_input("Insert the year",disabled = True)
+    
     if option == "Full date seting":
         date = st.date_input("Pick a date",disabled = False)
         st.write('Date :',date.year, date.month, date.day)
         
 
     elif option == "On month and year":
-        date.disabled = True
-        month.disabled = False
-        year.disabled = False
-        year_only.disabled = True
+        month = st.number_input("Insert the month")
+        year = st.number_input("Insert the year")
         st.write('Month :',int(month),'\t Year:', int(year))
     else:
-        date.disabled = True
-        month.disabled = True
-        year.disabled = True
-        year_only.disabled = False
+        year_only = st.number_input("Insert the year")
         st.write('Year :',year_only)
   
